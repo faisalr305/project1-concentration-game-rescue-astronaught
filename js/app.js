32fd0a15d45da434564dd33c360edffa736a12b2
FAISAL RAHIM <BAKHSH9182@Gmail.com>
2026-06-24 23:14:56 +0300

    added Cached Element References

---
+++ js/app.js
@@ -0,0 +1,15 @@
+/*------------------------ Cached Element References ------------------------*/
+const startBtn = document.querySelector("#startbtn");
+const rescueBtn = document.querySelector("#rescuebtn");
+const restartBtn = document.querySelector("#restartbtn");
+
+const oxygenBar = document.querySelector("#oxygen-bar");
+const oxygenValue = document.querySelector("#oxygen-value");
+const oxygenText = document.querySelector("#oxygen-text");
+
+const statusText = document.querySelector("#status");
+const targetText = document.querySelector("#target");
+const timerText = document.querySelector("#timer");
+const countdownText = document.querySelector("#countdown");
+const rocket = document.querySelector("#rocket");
+
