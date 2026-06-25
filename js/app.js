3dcb296e96aedaf72ac7996c89da6cd50196b073
FAISAL RAHIM <BAKHSH9182@Gmail.com>
2026-06-25 18:20:27 +0300

    added variables and startgame function

--- js/app.js
+++ js/app.js
@@ -12,4 +12,49 @@ const targetText = document.querySelector("#target");
 const timerText = document.querySelector("#timer");
 const countdownText = document.querySelector("#countdown");
 const rocket = document.querySelector("#rocket");
+/*-------------------------------- Variables --------------------------------*/
+let oxygen = 0;
+let timeLeft = 5;
+let targetMin = 0;
+let targetMax = 0;
+let gameActive = false;
+let oxygenInterval;
+let timerInterval;
+let countdownInterval;
 
+/*-------------------------------- Functions --------------------------------*/
+
+
+function startGame() {
+  clearAll();
+
+  oxygen = 0;
+  timeLeft = 5;
+  gameActive = false;
+
+  targetMin = Math.floor(Math.random() * 90);
+  targetMax = targetMin + 10;
+
+  targetText.textContent = Target: ${targetMin} - ${targetMax};
+  statusText.textContent = "Get Ready...";
+  timerText.textContent = "Time: 5";
+
+  oxygenBar.style.width = "0%";
+  oxygenValue.textContent = "0%";
+  oxygenText.textContent = "Oxygen Level: 0 / 100";
+
+  startCountdown();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/*----------------------------- Event Listeners -----------------------------*/
